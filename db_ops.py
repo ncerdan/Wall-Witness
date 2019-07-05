be7932169bf143edae5c5a88536a6b2f17067eb2
Nick Cerdan <ncerdan1229@gmail.com>
2019-07-04 18:08:48 -0700

    can now add session, workouts, and weights. sessions and workouts also check if they set new prs.

--- db_ops.py
+++ db_ops.py
@@ -14,33 +14,62 @@ db = client.get_database('wallwitness_db')
 sessions = db.sessions
 workouts = db.workouts
 weights  = db.weights
+prs = db.prs
 
 """ Functions """
 # Adding Documents
-def add_session(type_s, envr_s, avGr_s, hiGr_s, date_s, durn_s, locn_s, note_s):
+def add_session(type, envr, avGr, hiGr, date, durn, locn, note):
     """
-    Insert new session data into the sessions collection
-    * Params:
-        type of workout [bench, neg, pistol]    (string)
-        date for entry                          (datetime.date)
-        sets of exercise                        (int)
-        reps for ecah set of exercise           (float)
-        weight average                          (float)
-        weight maximum                          (float)
-    * Return:
-        none                                    (n/a)
+    Insert new session data into the sessions collection.
+    For rope climbing grades: a=.00,b=.25,c=.50,d=.75
+    Args:
+        type (string):        type of climbing ['boulder', 'toprope', 'sport'],
+        envr (string):        environment ['in', 'out'],
+        avGr (float):         average grade of session,
+        hiGr (float):         highest grade of session,
+        date (datetime.date): date for entry,
+        durn (float):         length of session,
+        locn (string):        location of session,
+        note (string):        notes of session.
+    Returns:
+        none.
     """
+    #marshalling data
+    type_i = str(type)
+    envr_i = str(envr)
+    avGr_i = float(avGr)
+    hiGr_i = float(hiGr)
+    date_i = date.strftime("%Y-%m-%d")
+    durn_i = float(durn)
+    locn_i = str(locn)
+    note_i = str(note)
+    to_insert = {
+        'type': type_i,
+        'envr': envr_i,
+        'avGr': avGr_i,
+        'hiGr': hiGr_i,
+        'date': date_i,
+        'durn': durn_i,
+        'locn': locn_i,
+        'note': note_i
+    }
+
+    #insert it
+    sessions.insert_one(to_insert)
+
+    #check if it is a new pr
+    check_new_pr(type_i, hiGr_i, date)
 
 def add_workout(type, date, sets, reps, avWt, hiWt):
     """
     Insert new workout data into the workouts collection.
     Args:
-        type (string): type of workout [bench, neg,  pistol],
+        type (string):        type of workout ['bench', 'neg',  'pistol'],
         date (datetime.date): date for entry,
-        sets (int): sets of exercise,
-        reps (float): reps for each set of exercise,
-        avWt (float): weight average,
-        hiWt (float): weight maximum.
+        sets (int):           sets of exercise,
+        reps (float):         reps for each set of exercise,
+        avWt (float):         weight average,
+        hiWt (float):         weight maximum.
     Returns:
         none.
     """
@@ -59,14 +88,19 @@ def add_workout(type, date, sets, reps, avWt, hiWt):
         'avWt': avWt_i,
         'hiWt': hiWt_i
     }
+
+    #insert it
     workouts.insert_one(to_insert)
+
+    #check if it is a new pr
+    check_new_pr(type_i, hiWt_i, date)
 
 def add_weight(date, wght):
     """
     Insert new weight data into the weights collection.
     Args:
         date (datetime.date): date for entry,
-        wght (float): weight for entry.
+        wght (float):         weight for entry.
     Returns:
         none.
     """
@@ -77,9 +111,52 @@ def add_weight(date, wght):
         'date': date_i,
         'wght': wght_i
     }
+
+    #insert it
     weights.insert_one(to_insert)
 
-# Updating Documents?
+# Updating Documents
+def check_new_pr(type, new_data, date):
+    """
+    Checks if new entry sets a new pr, and if it does updates it.
+    Args:
+        type (string):        type of pr ['boulder', 'toprope', 'sport', 'bench', 'neg', 'pistol'],
+        new_data (float):     new grade/weight pr,
+        date (datetime.date): date of new pr.
+    Returns:
+        none.
+    """
+
+    #check if type is bad?
+
+    #get the current pr of the given type
+    curr_pr = prs.find_one({'type': type})
+    curr_rd = curr_pr['rcrd']
+
+    #compare it to the new weight/grade
+    if new_data > curr_rd:
+        upd_pr(type, new_data, date)
+
+def upd_pr(type, rcrd, date):
+    """
+    Updates correct pr with new date and record.
+    Args:
+        type (string):        type of pr ['boulder', 'toprope', 'sport', 'bench', 'neg', 'pistol'],
+        rcrd (float):         new grade/weight pr,
+        date (datetime.date): date pr was attained.
+    Returns:
+        none.
+    """
+    #marshalling data
+    date_i = date.strftime("%Y-%m-%d")
+    to_update = {
+        'type': type,
+        'rcrd': rcrd,
+        'date': date_i
+    }
+
+    #update the correct pr's to the new data
+    prs.update({'type': type}, to_update)
 
 # Deleting Documents (how? -- based off of what?)
 def del_session():
--- wall_witness.py
+++ wall_witness.py
@@ -1,13 +1,8 @@
-# Note: write files in python3
-
 """ Imports """
 # Public
 import datetime
-
-# Private
 import db_ops
+#make update pr function and test with
+#   boulder/tr to set to 6/12
 
-""" TESTING """
-db_ops.add_workout('neg', datetime.date(2019, 7, 2), 2, 1, 0, 0)
-db_ops.add_workout('bench', datetime.date(2019, 7, 4), 3, 8, 115, 125)
-
+db_ops.add_workout('bench', datetime.date(2019, 2, 12), 3, 8, 115, 125)
